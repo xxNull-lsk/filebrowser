fd695ea308809b2402ca43451a5e2127f23a91ea
Allan <allan@163.com>
2021-02-05 15:36:44 +0800

    FIX: create file or folder always is 0755

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"runtime"
+	"syscall"
 
 	"github.com/filebrowser/filebrowser/v2/cmd"
 )
 
 func main() {
+
+	syscall.Umask(0) // FIX: Create file or dir mode always is 0755
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	cmd.Execute()
 }
